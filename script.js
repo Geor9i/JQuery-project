7fdb4ed270a8879ed180b7685afc9089013ad40b
Georgi Urumov <gurumov93@gmail.com>
2024-04-21 17:03:06 +0100

    border loop animation

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let i = 0
-let maxCount = 2;
+let maxCount = 4;
 const loop = () => {
     i = (i + 1) % maxCount;
     let borderWidth = i === 1 ? '10px' : '0'; // Set border width based on current value of i
-    $('.box').animate({
-    borderWidth: borderWidth,
-    borderColor: 'red'
-}, 500, loop)
+    const border = ['top', 'right', 'bottom', 'left'];
+    const borderWidthProp = `border-${border[i]}-width`;
+    const borderColorProp = `border-${border[i]}-color`;
+    $('.box').eq(1).css('border', '8px solid black').animate({
+    [borderWidthProp] : '8px',
+    [borderColorProp] : 'red',
+}, 1000, loop)
 };
 
 loop()
