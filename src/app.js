ab48d1038bdc51b9ae6b32299dc9c46e51040b19
Georgi <gurumov93@gmail.com>
2024-04-23 19:52:37 +0100

    Install Decimal.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import styles from "../styles/site.scss";
 import "jquery";
 import "jquery-ui";
+import Decimal from "decimal.js";
+
 import { loop } from "./components/cubeAnimation.js";
 
 if (module.hot) {
   module.hot.accept();
 }
+
+const x = new Decimal(2.22323).ceil().valueOf();
+console.log(x);
 
 loop();
