826e48ae8ef1dced1f78725d617d414f46d931a2
Georgi <gurumov93@gmail.com>
2024-04-23 19:02:05 +0100

    Fixed HMR on HTML

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, "dist"),
     },
+    watchFiles: ["*.html"],
     compress: true,
     open: true,
     hot: true, // Enable HMR
@@ -71,7 +72,6 @@ module.exports = {
       "jQuery":"jquery",
       "window.jQuery":"jquery"
     }),
-    new webpack.HotModuleReplacementPlugin()
   ],
   optimization: {
     splitChunks: {
